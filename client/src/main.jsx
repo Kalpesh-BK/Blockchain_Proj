c35d37c5489c79778ee57edd0857ea37a3479408
Kalpesh-BK <kalpeshbaviskar315@gmail.com>
2023-03-15 15:45:19 +0530

    Commit

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router } from 'react-router-dom';
+// import { ThirdwebProvider } from "@3rdweb/react";
+import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
+import './index.css';
+
+import App from './App';
+import { StateContextProvider } from './context';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    
+    <ThirdwebProvider desireChainId={ChainId.Goerli}>
+        <Router>
+            <StateContextProvider>
+            <App />
+            </StateContextProvider>
+           
+        </Router>
+     </ThirdwebProvider>
+    
+)
